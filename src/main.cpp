6d51e61c9ae4d74c7d4afe356d90e6fa7dc98150
newRT <cgn00b2020@gmail.com>
2020-02-23 23:33:40 +0800

    Set basic header files and create glfw Window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
+// common lib
+#include <assert.h>
 #include <stdio.h>
+
+// GLFW for window create
+#include <GLFW/glfw3.h>
+
+// vulkan header file
+#include <vulkan/vulkan.h>
 
 int main()
 {
 	printf("hello, vulkan!\n");
+
+	int ret = glfwInit();
+	assert(ret);
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "My Renderer", 0, 0);
+	assert(window);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
 
 	return 0;
 }
